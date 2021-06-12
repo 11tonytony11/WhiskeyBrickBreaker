ca79c74dbe033c6fb1e7d73279cb7c4eedbcd1e4
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-06-12 15:22:02 +0300

    Added comments to the updater class

--- Updater.java
+++ Updater.java
@@ -1,15 +1,23 @@
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Updater implements ActionListener
 {
     BallsAndBoxesPanel panel;
-
+    /* ------------------------------------------------------------------
+     * This function is the Updater c'tor
+     * Input:  game panel
+     * Output: None
+    /* ------------------------------------------------------------------ */
     public Updater(BallsAndBoxesPanel newPanel)
     {
         this.panel = newPanel;
     }
-
+    /* ------------------------------------------------------------------
+     * This function is handling the repainting when timer is calling the event
+     * Input:  e - event
+     * Output: None
+    /* ------------------------------------------------------------------ */
     @Override
     public void actionPerformed(ActionEvent e)
     {
