3e1e1239f861daac3d7268df70785ca21f89db4a
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-06-13 13:37:34 +0300

    Refactored file

--- AePlayWave.java
+++ AePlayWave.java
@@ -1,73 +1,66 @@
-import java.io.File;
-import java.io.IOException;
-import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.io.File;
 
-public class AePlayWave extends Thread {
 
-	private String filename;
-
-	private Position curPosition;
+public class AePlayWave extends Thread
+{
 
 	private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
+	enum Position { LEFT, RIGHT, NORMAL };
+	private Position curPosition;
+	private String filename;
 
-	enum Position {
-		LEFT, RIGHT, NORMAL
-	};
-
-	public AePlayWave(String wavfile) {
+	/* ------------------------------------------------------------------
+ 	 * This function is the Player c'tor
+ 	 * Input:  path to .wav file
+ 	 * Output: None
+	/* ------------------------------------------------------------------ */
+	public AePlayWave(String wavfile)
+	{
 		filename = wavfile;
 		curPosition = Position.NORMAL;
 	}
-
-	public AePlayWave(String wavfile, Position p) {
-		filename = wavfile;
-		curPosition = p;
-	}
-
-	public void run() {
-
+	/* ------------------------------------------------------------------
+     * This function runs the audio file
+ 	 * Input:  None
+ 	 * Output: Plays the sound
+	/* ------------------------------------------------------------------ */
+	public void run()
+	{
 		File soundFile = new File(filename);
-		if (!soundFile.exists()) {
+		AudioInputStream audioInputStream;
+		SourceDataLine auline;
+		
+		if (!soundFile.exists())
+		{
 			System.err.println("Wave file not found: " + filename);
 			return;
 		}
-
-		AudioInputStream audioInputStream = null;
-		try {
+		
+		try
+		{
 			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-		} catch (UnsupportedAudioFileException e1) {
-			e1.printStackTrace();
-			return;
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			return;
-		}
-
-		AudioFormat format = audioInputStream.getFormat();
-		SourceDataLine auline = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-
-		try {
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioInputStream.getFormat());
 			auline = (SourceDataLine) AudioSystem.getLine(info);
-			auline.open(format);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			return;
-		} catch (Exception e) {
-			e.printStackTrace();
+			auline.open(audioInputStream.getFormat());
+		}
+		catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1)
+		{
+			e1.printStackTrace();
 			return;
 		}
-
-		if (auline.isControlSupported(FloatControl.Type.PAN)) {
-			FloatControl pan = (FloatControl) auline
-					.getControl(FloatControl.Type.PAN);
+		
+		if (auline.isControlSupported(FloatControl.Type.PAN))
+		{
+			FloatControl pan = (FloatControl) auline.getControl(FloatControl.Type.PAN);
 			if (curPosition == Position.RIGHT)
 				pan.setValue(1.0f);
 			else if (curPosition == Position.LEFT)
@@ -78,19 +71,23 @@ public class AePlayWave extends Thread {
 		int nBytesRead = 0;
 		byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
 
-		try {
-			while (nBytesRead != -1) {
+		try 
+		{
+			while (nBytesRead != -1) 
+			{
 				nBytesRead = audioInputStream.read(abData, 0, abData.length);
 				if (nBytesRead >= 0)
 					auline.write(abData, 0, nBytesRead);
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
-			return;
-		} finally {
+		}
+		finally 
+		{
 			auline.drain();
 			auline.close();
 		}
-
 	}
 }
