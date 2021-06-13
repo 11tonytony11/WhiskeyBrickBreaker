fd8aa9da6afe932bc8bdc422ebf8e5b555e7d328
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2021-06-13 13:44:20 +0300

    Refactored ball

--- Ball.java
+++ Ball.java
@@ -9,6 +9,11 @@ public class Ball extends Thread
     Color color;
     Timer timer;
 
+	/* ------------------------------------------------------------------
+ 	 * This function is the ball c'tor
+ 	 * Input:  (x,y) location, width, color and panel
+ 	 * Output: None
+	/* ------------------------------------------------------------------ */
 	public Ball(int x, int y, int width, Color color, BallsAndBoxesPanel p)
 	{
 		this.color = color;
@@ -18,15 +23,23 @@ public class Ball extends Thread
 		this.y = y;
 
 		this.timer = new Timer(5, new Updater(this.panel));
-		timer.start();
+		this.timer.start();
 	}
-	
+	/* ------------------------------------------------------------------
+ 	 * This function draws the racket
+ 	 * Input:  software graphics
+ 	 * Output: None
+	/* ------------------------------------------------------------------ */
 	public void draw(Graphics g)
 	{
 		g.setColor(color);
 		g.fillOval(x - width / 2,y - width / 2,width,width);
 	}
-	
+	/* ------------------------------------------------------------------
+ 	 * This function handles the ball behavior
+ 	 * Input:  None
+ 	 * Output: None
+	/* ------------------------------------------------------------------ */
 	public void run()
 	{
 	    dirx = 1;
@@ -57,10 +70,10 @@ public class Ball extends Thread
 
 				if(y + width / 2  >= h)
 				{
+
 					JOptionPane.showMessageDialog(null, "You lost", "Game over", JOptionPane.INFORMATION_MESSAGE);
 					System.exit(0);
 				}
-
 				try
 				{
 					Thread.sleep(5);
